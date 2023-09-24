a27abf5d32400b5a9359abe07c0a0ce54521b7d2
Nhooearn <99580726+Nhooearn@users.noreply.github.com>
2023-09-25 00:08:03 +0700

    v.1.1

--- insert_std.php
+++ insert_std.php
@@ -15,23 +15,29 @@ $dbname = "students";
 // create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
-    die("Connection failed " . mysqli_connect_error());
+  die("Connection failed " . mysqli_connect_error());
+  // ...
 } else {
-  echo "<script>
-          alert('ID ถูกใช้ไปแล้ว');
-          window.location.href = 'student.php';
-        </script>";
+  $check_sql = "SELECT * FROM `std_info` WHERE `id` = '$id'";
+  $check_result = mysqli_query($conn, $check_sql);
+  if (mysqli_num_rows($check_result) > 0) {
+    echo "<script>
+            alert('ID already exists!');
+            window.location.href = 'student.php';
+            </script>";
+  }
 }
+
 $sql = "INSERT INTO `std_info` (`id`, `en_name`, `en_surname`, `th_name`, `th_surname`, `major_code`, `email`) VALUES ('$id', '$en_name', '$en_surname', '$th_name', '$th_surname', '$major_code', '$email')";
 //echo $sql."<br>";
 $result = mysqli_query($conn, $sql);
 if ($result) {
-    echo "<script>
+  echo "<script>
             alert('Record updated successfully!');
             window.location.href = 'student.php';
           </script>";
 } else {
-    echo "<script>
+  echo "<script>
             alert('Error: " . mysqli_error($conn) . "');
             window.location.href = 'student.php';
           </script>";
